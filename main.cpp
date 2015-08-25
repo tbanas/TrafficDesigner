b692ee4326159c6d59a4fd82c037a9bbdc4679c9
Tomasz Banaś <tomek.kacper@gmail.com>
2015-08-25 17:13:11 +0200

    File structure refactored

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+#include <allegro5\allegro.h>
+#include <stdio.h>
+
+#include "Render.h"
+#include "UserInterface.h"
+#include "Intersection.h"
+#include "Lane.h"
+#include "Road.h"
+
+#include "Button.h"
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-	cout << "Sample" << endl;
+	UserInterface* UI = new UserInterface;
+	UI->init();
+	while (1)
+	{
+		UI->update();
+	}
+	
+	system("pause");
 	return 0;
 }
